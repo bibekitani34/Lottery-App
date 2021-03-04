86b3d3bce842613ea06131ea3ac76fa477ea9637
Bibek Itani <bibekitani34@gmail.com>
2021-03-04 18:00:10 -0500

    Initial Commit

---
+++ Lottery App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lottery App
+//
+//  Created by Bibek Itani on 3/4/21.
+//  Copyright © 2021 Bibek ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
