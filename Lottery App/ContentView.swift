4a4270a708ea02849d5819aa92685fd4a0f79ede
Bibek Itani <bibekitani34@gmail.com>
2021-03-04 19:02:42 -0500

    Committed!

--- Lottery App/ContentView.swift
+++ Lottery App/ContentView.swift
@@ -9,10 +9,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+
+     @State var rand1 = 1
+     @State var rand2 = 1
+     @State var rand3 = 1
+     @State var score = 1000
+    
     var body: some View {
-        Text("Hello, World!")
+       
+            VStack{
+                Spacer()
+                Text("Lottery Game").font(.largeTitle)
+                Spacer()
+                    .font(.system(size: 40))
+                    
+               
+                HStack{
+                    Text("Credits:  ")
+                    
+                    .bold()
+                    
+                    Text(String(score))
+                }
+               
+                HStack{
+                  
+                    Image("fruit\(rand1)")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    
+                    Image("fruit\(rand2)")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    Image("fruit\(rand3)")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                  
+                }
+                Spacer()
+                Button(action: {
+                    
+                    self.rand1 = Int.random(in: 1...3)
+                    self.rand2 = Int.random(in: 1...3)
+                    self.rand3 = Int.random(in: 1...3)
+                    
+                    if self.rand1 == self.rand2 && self.rand2 == self.rand3{
+                        self.score += 50
+                    }else{
+                        self.score -= 20
+                    }
+                }) {
+                    Text("Spin").padding().padding([.leading, .trailing], 40).background(Color.pink).cornerRadius(25).foregroundColor(Color.white)
+                
+                }
+            Spacer()
+                Button(action:{
+                    self.score = 1000
+                }, label: {
+                    Text("Reset game")
+                        .padding().padding([.leading, .trailing], 40).background(Color.green).cornerRadius(25).foregroundColor(Color.white)
+                })
+                Spacer()
+                
+                
+                
+                
+                }
+            
+        }
+                       
+               
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
